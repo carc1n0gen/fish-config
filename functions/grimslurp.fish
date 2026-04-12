3816b8e954496219f185ebddfe933fbc5fbde3f0
Carson Evans <carsonwevans@gmail.com>
2026-04-12 07:22:20 -0400

    fix image attachment for screenshot notification

--- functions/grimslurp.fish
+++ functions/grimslurp.fish
@@ -1,8 +1,18 @@
 function grimslurp
-   if command -q grim && command -q slurp && command -q wl-copy
-       set -l IMG ~/Pictures/Screenshots/(date +%Y-%m-%d_%H:%M:%S).png
-       grim -g (slurp) $IMG && wl-copy < $IMG
-   else
-       echo "grim, slurp, and wl-copy are required for grimslurp to work."
-   end
+    set -l message ""
+    set -l image_path ""
+    set -l date_fmt (set -q SCREENSHOT_DATE_FORMAT; and echo $SCREENSHOT_DATE_FORMAT; or echo "%FT%T")
+    if command -q grim && command -q slurp && command -q wl-copy
+        set image_path ~/Pictures/Screenshots/(date +$date_fmt).png
+        grim -g (slurp) $image_path && wl-copy < $image_path
+        set message "Imaged saved to $image_path and copied to clipboard."
+    else
+        set message "grim, slurp, and wl-copy are required for grimslurp to work."
+    end
+
+    if status is-interactive
+        echo $message
+    else
+        notify-send "Screenshot saved" "$message" -i "$image_path" -a "Screenshot"
+    end
 end
